2c2c5a4ed49def8c7f46dbfaa9b5dbc898be2c8c
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-05-08 15:19:02 +0300

    Fix formatting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const router = require("express-file-routing")
+const router = require('express-file-routing')
 
 dotenv.config()
-
 
 const app = express()
 const PORT = process.env.PORT
 
-app.use("/",router.router())
+app.use('/', router.router())
 
 app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`)
--- backend/src/routes/version.ts
+++ backend/src/routes/version.ts
@@ -1,6 +1,5 @@
-
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 
 export const get = async (req: Request, res: Response) => {
-    res.json({"version" : process.env.npm_package_version})
+    res.json({ version: process.env.npm_package_version })
 }
