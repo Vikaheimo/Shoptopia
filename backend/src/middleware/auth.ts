21e62ab344cd30a03094986f3d75dbae36388fb1
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-05-09 14:43:39 +0300

    Add middleware to check if user is authenticated

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express'
+import { getSubjectJWT } from '../utils/jwtHandler'
+
+/**
+ * Middleware witch checks jwt token from header named "bearer".
+ */
+export default () => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let bearer = req.headers.bearer
+        if (!bearer || typeof bearer === 'object' || !getSubjectJWT(bearer)) {
+            res.status(401).json({error: "Authorization error"})
+            return
+        } 
+        next()
+    } catch (error) {
+        res.status(500).json({
+            error: "Internal server error"
+        })
+    }
+}
